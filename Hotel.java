dcfc7fdeed7b71012fba6cbfac6b024e0a1acdb5
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-10-17 18:45:10 +0530

    [abhishek] add: ability to add rating to each hotel

--- Hotel.java
+++ Hotel.java
@@ -5,39 +5,58 @@ public class Hotel {
 	private int weekdayRateForRegularCustomer;
 	private int weekendRateForRegularCustomer;
 	private int totalPrice;
+	private int rating;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Hotel() {
-		
+
 	}
+
 	public Hotel(String name, int weekdayRateForRegularCustomer, int weekendRateForRegularCustomer) {
 		super();
 		this.name = name;
 		this.weekdayRateForRegularCustomer = weekdayRateForRegularCustomer;
 		this.weekendRateForRegularCustomer = weekendRateForRegularCustomer;
-		this.totalPrice=0;
+		this.totalPrice = 0;
 	}
+
 	public int getTotalPrice() {
 		return totalPrice;
 	}
+
 	public void setTotalPrice(int totalPrice) {
 		this.totalPrice = totalPrice;
 	}
+
 	public int getWeekdayRateForRegularCustomer() {
 		return weekdayRateForRegularCustomer;
 	}
+
 	public void setWeekdayRateForRegularCustomer(int weekdayRateForRegularCustomer) {
 		this.weekdayRateForRegularCustomer = weekdayRateForRegularCustomer;
 	}
+
 	public int getWeekendRateForRegularCustomer() {
 		return weekendRateForRegularCustomer;
 	}
+
 	public void setWeekendRateForRegularCustomer(int weekendRateForRegularCustomer) {
 		this.weekendRateForRegularCustomer = weekendRateForRegularCustomer;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
 	}
 
 }
--- HotelReservationSystem.java
+++ HotelReservationSystem.java
@@ -26,6 +26,8 @@ public class HotelReservationSystem {
 			hotel.setWeekdayRateForRegularCustomer(consoleInputObj.nextInt());
 			System.out.println("Enter the Weekend Rate for Regular Customer");
 			hotel.setWeekendRateForRegularCustomer(consoleInputObj.nextInt());
+			System.out.println("Enter the Rating");
+			hotel.setRating(consoleInputObj.nextInt());
 			hotelList.add(hotel);
 			System.out.println("Do you want to Add more Hotel in the list(Y/N)");
 			char choice = consoleInputObj.next().charAt(0);
@@ -34,59 +36,60 @@ public class HotelReservationSystem {
 		}
 	}
 
-	public static void findCheapestHotelGivenDateRange() throws ParseException{
-	    	calculateTotalPrice();
-	    	Hotel hotelListOfMinimumPrice = hotelList.stream()
-					.min((hotel1, hotel2) -> hotel1.getTotalPrice() > hotel2.getTotalPrice() ? 1 : -1).get();
-	    	List<Hotel> desiredHotelList = hotelList.stream()
-					.filter(hotel -> hotel.getTotalPrice() == hotelListOfMinimumPrice.getTotalPrice()).collect(Collectors.toList());
-	    	desiredHotelList.stream().forEach(hotel->System.out.print(hotel.getName()
-			+ "   "));
-	    	System.out.println("with Total Rates $"+hotelListOfMinimumPrice.getTotalPrice() );
-		   
-	    }
-	public static void  calculateTotalPrice() throws ParseException {
+	public static void findCheapestHotelGivenDateRange() throws ParseException {
+		calculateTotalPrice();
+		Hotel hotelListOfMinimumPrice = hotelList.stream()
+				.min((hotel1, hotel2) -> hotel1.getTotalPrice() > hotel2.getTotalPrice() ? 1 : -1).get();
+		List<Hotel> desiredHotelList = hotelList.stream()
+				.filter(hotel -> hotel.getTotalPrice() == hotelListOfMinimumPrice.getTotalPrice())
+				.collect(Collectors.toList());
+		desiredHotelList.stream().forEach(hotel -> System.out.print(hotel.getName() + "   "));
+		System.out.println("with Total Rates $" + hotelListOfMinimumPrice.getTotalPrice());
+
+	}
+
+	public static void calculateTotalPrice() throws ParseException {
 		Scanner scannerObj = new Scanner(System.in);
 		System.out.println("Enter the Custome type ");
 		String customerType = scannerObj.next();
-	    int days[]=getDateFromUserAndReturnDay();
+		int days[] = getDateFromUserAndReturnDay();
 		for (Hotel hotel : hotelList) {
 			if (customerType.equals("RegularCustomer")) {
-				   for(int index=0;days[index]!=0;index++) {
-					   if ((days[index] == 1 ||days[index]  == 7))
-							hotel.setTotalPrice(hotel.getTotalPrice() + hotel.getWeekendRateForRegularCustomer());
-						else
-							hotel.setTotalPrice(hotel.getTotalPrice() + hotel.getWeekdayRateForRegularCustomer());   
-				   }
+				for (int index = 0; days[index] != 0; index++) {
+					if ((days[index] == 1 || days[index] == 7))
+						hotel.setTotalPrice(hotel.getTotalPrice() + hotel.getWeekendRateForRegularCustomer());
+					else
+						hotel.setTotalPrice(hotel.getTotalPrice() + hotel.getWeekdayRateForRegularCustomer());
+				}
 			}
 		}
-    }
-	public static int[] getDateFromUserAndReturnDay() throws ParseException
-	{
-		int index=0;
-		int day[]=new int[10];
-		while(true)
-		{
-		System.out.println("Enter the date in format ddMMMyyyy");
-		Scanner consoleInputObj = new Scanner(System.in);
-		Calendar calendar = Calendar.getInstance();
-		String date1 = consoleInputObj.nextLine();
-		SimpleDateFormat format = new SimpleDateFormat("ddMMMyyyy");
-		Date yourDate = format.parse(date1);
-		calendar.setTime(yourDate);
-		day[index] = calendar.get(Calendar.DAY_OF_WEEK);
-		index++;
-		System.out.println("Do you want to stay more day(y/n)");
-		char choice=consoleInputObj.next().charAt(0);
-		if(choice=='n'||choice=='N')
-			break;
-		else if(choice=='y'||choice=='Y')
-			continue;
-		else
-		   System.out.println("invalid input");
+	}
+
+	public static int[] getDateFromUserAndReturnDay() throws ParseException {
+		int index = 0;
+		int day[] = new int[10];
+		while (true) {
+			System.out.println("Enter the date in format ddMMMyyyy");
+			Scanner consoleInputObj = new Scanner(System.in);
+			Calendar calendar = Calendar.getInstance();
+			String date1 = consoleInputObj.nextLine();
+			SimpleDateFormat format = new SimpleDateFormat("ddMMMyyyy");
+			Date yourDate = format.parse(date1);
+			calendar.setTime(yourDate);
+			day[index] = calendar.get(Calendar.DAY_OF_WEEK);
+			index++;
+			System.out.println("Do you want to stay more day(y/n)");
+			char choice = consoleInputObj.next().charAt(0);
+			if (choice == 'n' || choice == 'N')
+				break;
+			else if (choice == 'y' || choice == 'Y')
+				continue;
+			else
+				System.out.println("invalid input");
 		}
 		return day;
 	}
+
 	public static void main(String[] args) throws ParseException {
 		addHotelRateAndNameInHotelReservationSystem();
 		findCheapestHotelGivenDateRange();
